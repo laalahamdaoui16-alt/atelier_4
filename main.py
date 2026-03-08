7856b4c5b7286cc85068b7d943e0473959ee21c0
laala <hamdaouilaala2@gmail.com>
2026-03-08 19:04:30 -0400

    correction de qlq erreurs

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-from encodings.punycode import selective_find
-
 
 class employe:
     def __init__(self,nom,prenom):
@@ -9,7 +7,7 @@ class employe:
     def afficher_info(self):
         info = f"employe: {self.nom}, prenom: {self.prenom}"
         if self.voiture:
-            info += f",voiture attribuee: {self.voiture.marque} {self.voiture.model} {self.voiture.imatriculation}"
+            info += f",voiture attribuee: {self.voiture.marque} {self.voiture.modele} {self.voiture.immatriculation}"
         else:
             info += ",pas de voiture attribuee"
         print(info)
@@ -17,26 +15,26 @@ class employe:
         if self.voiture:
             print(f"{self.nom} {self.prenom} a deja une voiture")
         elif voiture.employe:
-            print (f"la voiture {voiture.marque} {voiture.model} est deja attribuee a {voiture.employe.nom} {voiture.employe.prenom}")
+            print (f"la voiture {voiture.marque} {voiture.modele} est deja attribuee a {voiture.employe.nom} {voiture.employe.prenom}")
         else:
             self.voiture=voiture
             voiture.employe=self
             print(f"Voiture {voiture.marque} {voiture.modele} attribuée à {self.nom} {self.prenom}")
     def retirer_voiture(self):
         if self.voiture:
-            print(f"voiture {self.voiture.marque} {self.voiture.model} retirer voiture de {self.nom} {self.prenom}")
+            print(f"voiture {self.voiture.marque} {self.voiture.modele} retirer voiture de {self.nom} {self.prenom}")
             self.voiture.employe= None
             self.voiture= None
         else:
             print(f"{self.nom} {self.prenom} n a pas de voiture a retirer")
 class voiture:
-    def __init__(self,marque,model,imatriculation):
+    def __init__(self,marque,modele,immatriculation):
         self.marque = marque
-        self.model = model
-        self.imatriculation = imatriculation
+        self.modele = modele
+        self.immatriculation = immatriculation
         self.employe = None
     def afficher_info(self):
-        info = f"voiture: {self.marque} {self.model} {self.imatriculation}"
+        info = f"voiture: {self.marque} {self.modele} {self.immatriculation}"
         if self.employe:
             info  += (f",attribuee a : {self.employe.nom} {self.employe.prenom}")
         else:
@@ -55,7 +53,7 @@ print("info initial des employes")
 for e in [e1,e2,e3]:
     e.afficher_info()
 print("infos initial des voitures")
-for v in [v1,v2]:
+for v in [v1,v2,v3]:
     v.afficher_info()
 
 e1.attribuer_voiture(v1)
@@ -63,14 +61,14 @@ e2.attribuer_voiture(v2)
 print("infos apres attributions")
 for e in [e1,e2,e3]:
     e.afficher_info()
-for v in [v1,v2]:
+for v in [v1,v2,v3]:
     v.afficher_info()
 
 e1.retirer_voiture()
 print("infos apres retires voitures")
 for e in [e1,e2,e3]:
     e.afficher_info()
-for v in [v1,v2]:
+for v in [v1,v2,v3]:
     v.afficher_info()
 
 e3.attribuer_voiture(v2)
