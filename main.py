d4f87944452eeb27996bf1d295236fddcee304a3
laala <hamdaouilaala2@gmail.com>
2026-03-09 17:30:27 -0400

    ajout de quelques attributs

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 
 class employe:
-    def __init__(self,nom,prenom):
+    def __init__(self,nom,prenom,npermis):
         self.nom = nom
         self.prenom = prenom
+        self.npermis = npermis
         self.voiture = None
     def afficher_info(self):
-        info = f"employe: {self.nom}, prenom: {self.prenom}"
+        info = f"employe: {self.nom}, prenom: {self.prenom} {self.npermis}"
         if self.voiture:
             info += f",voiture attribuee: {self.voiture.marque} {self.voiture.modele} {self.voiture.immatriculation}"
         else:
@@ -13,9 +14,9 @@ class employe:
         print(info)
     def attribuer_voiture(self,voiture):
         if self.voiture:
-            print(f"{self.nom} {self.prenom} a deja une voiture")
+            print(f"{self.nom} {self.prenom} {self.npermis}a deja une voiture")
         elif voiture.employe:
-            print (f"la voiture {voiture.marque} {voiture.modele} est deja attribuee a {voiture.employe.nom} {voiture.employe.prenom}")
+            print (f"la voiture {voiture.marque} {voiture.modele} est deja attribuee a {voiture.employe.nom} {voiture.employe.prenom} {voiture.employe.npermis}")
         else:
             self.voiture=voiture
             voiture.employe=self
@@ -26,28 +27,30 @@ class employe:
             self.voiture.employe= None
             self.voiture= None
         else:
-            print(f"{self.nom} {self.prenom} n a pas de voiture a retirer")
+            print(f"{self.nom} {self.prenom} {self.npermis} n a pas de voiture a retirer")
 class voiture:
-    def __init__(self,marque,modele,immatriculation):
+    def __init__(self,marque,modele,immatriculation,annee,km):
         self.marque = marque
         self.modele = modele
+        self.annee = annee
+        self.km = km
         self.immatriculation = immatriculation
         self.employe = None
     def afficher_info(self):
-        info = f"voiture: {self.marque} {self.modele} {self.immatriculation}"
+        info = f"voiture: {self.marque} {self.modele} {self.immatriculation} {self.annee} {self.km}"
         if self.employe:
-            info  += (f",attribuee a : {self.employe.nom} {self.employe.prenom}")
+            info  += (f",attribuee a : {self.employe.nom} {self.employe.prenom} {self.employe.npermis }")
         else:
             info += ",pas d,employe attribuee"
         print(info)
 
-e1=employe("hamdaoui","laala")
-e2=employe("bourj","lina")
-e3=employe("boukhalfa","ali")
+e1=employe("hamdaoui","laala","h35409")
+e2=employe("bourj","lina","l08567")
+e3=employe("boukhalfa","ali","g56439")
 
-v1=voiture("toyota","yaris","X50 MPC")
-v2=voiture("renault","clio","y50 oPC")
-v3=voiture("peugeot","207","R65 GEK")
+v1=voiture("toyota","yaris","X50 MPC","2007","139000")
+v2=voiture("renault","clio","y50 oPC","2009","140000")
+v3=voiture("peugeot","207","R65 GEK","2012","120999")
 
 print("info initial des employes")
 for e in [e1,e2,e3]:
